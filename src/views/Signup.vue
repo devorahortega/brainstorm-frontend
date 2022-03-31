72803594bc4255807db48f5ec41e1a5866df235a
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-31 13:57:44 -0500

    Fixes the form in the signin page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,40 +1,44 @@
 <template>
   <div class="signup">
     <template>
-      <h2>Create an Account</h2>
-      <h3>Name</h3>
-      <input type="text" name="first_name" class="form-control" id="first_name" placeholder="First Name" required />
-      <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Last Name" required />
-      <h3>Email</h3>
-      <input
-        type="email"
-        class="form-control"
-        name="email"
-        id="email"
-        placeholder="Email"
-        required
-        v-model="newUserParams.email"
-      />
-      <h3>Password</h3>
-      <input
-        type="text"
-        class="form-control"
-        name="password"
-        id="password"
-        placeholder="Password"
-        required
-        v-model="newUserParams.password"
-      />
-      <input
-        type="text"
-        class="form-control"
-        name="password_confirmation"
-        id="password_confirmation"
-        placeholder="Confirm Password"
-        required
-        v-model="newUserParams.password_confirmation"
-      />
-      <div class="text-center"><button type="submit" value="Submit">Create Account</button></div>
+      <form>
+        <div>
+          <h2>Create an Account</h2>
+          <h3>Name</h3>
+          <input type="text" name="first_name" class="form-control" id="first_name" placeholder="First Name" required />
+          <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Last Name" required />
+          <h3>Email</h3>
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+            v-model="newUserParams.email"
+          />
+          <h3>Password</h3>
+          <input
+            type="text"
+            class="form-control"
+            name="password"
+            id="password"
+            placeholder="Password"
+            required
+            v-model="newUserParams.password"
+          />
+          <input
+            type="text"
+            class="form-control"
+            name="password_confirmation"
+            id="password_confirmation"
+            placeholder="Confirm Password"
+            required
+            v-model="newUserParams.password_confirmation"
+          />
+          <div class="text-center"><button type="submit" value="Submit">Create Account</button></div>
+        </div>
+      </form>
     </template>
   </div>
 </template>
