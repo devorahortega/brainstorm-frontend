6f08cf2b2c1a5425424e5261121ac3f109020f55
Devorah Ortega <devorahb.ortega@gmail.com>
2022-04-06 00:16:40 -0500

    Fixes some issues in the signup page -its a backend issue...

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,45 +1,52 @@
 <template>
   <div class="signup">
-    <template>
-      <form>
-        <div>
-          <h2>Create an Account</h2>
-          <h3>Name</h3>
-          <input type="text" name="first_name" class="form-control" id="first_name" placeholder="First Name" required />
-          <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Last Name" required />
-          <h3>Email</h3>
-          <input
-            type="email"
-            class="form-control"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-            v-model="newUserParams.email"
-          />
-          <h3>Password</h3>
-          <input
-            type="text"
-            class="form-control"
-            name="password"
-            id="password"
-            placeholder="Password"
-            required
-            v-model="newUserParams.password"
-          />
-          <input
-            type="text"
-            class="form-control"
-            name="password_confirmation"
-            id="password_confirmation"
-            placeholder="Confirm Password"
-            required
-            v-model="newUserParams.password_confirmation"
-          />
-          <div class="text-center"><button type="submit" value="Submit">Create Account</button></div>
-        </div>
-      </form>
-    </template>
+    <form v-on:submit.prevent="submit()">
+      <h2>Create an Account</h2>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Name:</label>
+        <input type="text" name="first_name" class="form-control" id="first_name" placeholder="First Name" required />
+        <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Last Name" required />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          class="form-control"
+          name="email"
+          id="email"
+          placeholder="Email"
+          required
+          v-model="newUserParams.email"
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="text"
+          class="form-control"
+          name="password"
+          id="password"
+          placeholder="Password"
+          required
+          v-model="newUserParams.password"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          class="form-control"
+          name="password_confirmation"
+          id="password_confirmation"
+          placeholder="Confirm Password"
+          required
+          v-model="newUserParams.password_confirmation"
+        />
+      </div>
+      <div class="text-center"><button type="submit" value="Submit">Create Account</button></div>
+    </form>
   </div>
 </template>
 
@@ -48,8 +55,9 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      newUserParams: {},
+      newUserParams: { password: "", password_confirmation: "" },
       errors: [],
+      newSessionParams: {},
     };
   },
   methods: {
