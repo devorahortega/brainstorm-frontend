930d9e9d2b4fbd3721704e82e84d404ec29861c4
Devorah Ortega <devorahb.ortega@gmail.com>
2022-04-13 16:24:43 -0400

    Adds bootstrap to ideas, and app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">
+  <div id="#app">
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
     <div id="nav">
       <router-link to="/">Home</router-link>
       |
@@ -19,19 +22,23 @@
     </div>
     <router-view />
 
+    <title>BrainSTORM</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top">
       <div class="container d-flex align-items-center">
-        <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
+        <h1 class="logo me-auto"><a href="index.html">BrainStorm</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid" /></a>
 
         <nav id="navbar" class="navbar">
           <ul>
-            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-            <li><a class="nav-link scrollto" href="#about">About</a></li>
-            <li><a class="nav-link scrollto" href="#services">Services</a></li>
-            <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+            <li><a class="nav-link scrollto active" href="/">Home</a></li>
+            <li><a class="nav-link scrollto" href="/cards">About</a></li>
+            <li><a class="nav-link scrollto" href="/create">Services</a></li>
+            <li><a class="nav-link scrollto" href="/ideas">Portfolio</a></li>
             <li><a class="nav-link scrollto" href="#team">Team</a></li>
             <li class="dropdown">
               <a href="#">
@@ -42,20 +49,15 @@
                 <li><a href="#">Drop Down 1</a></li>
                 <li class="dropdown">
                   <a href="#">
-                    <span>Deep Drop Down</span>
+                    <span>Account</span>
                     <i class="bi bi-chevron-right"></i>
                   </a>
                   <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
+                    <li><a href="/signup">Signup</a></li>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/logout">Logout</a></li>
                   </ul>
                 </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
               </ul>
             </li>
             <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
