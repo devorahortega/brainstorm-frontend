9acdbfc9812c534f889ce0da49233a734782b78b
Devorah Ortega <devorahb.ortega@gmail.com>
2022-04-18 11:49:05 -0400

    Adds links to nabaar.

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,34 @@
   <div id="app">
     <header id="header" class="fixed-top">
       <div class="container d-flex align-items-center">
-        <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
+        <h1 class="logo me-auto"><a href="index.html">BrainSTORM</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
         <nav id="navbar" class="navbar">
           <ul>
-            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-            <li><a class="nav-link scrollto" href="#about">About</a></li>
-            <li><a class="nav-link scrollto" href="#services">Services</a></li>
-            <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-            <li><a class="nav-link scrollto" href="#team">Team</a></li>
+            <li><a class="nav-link scrollto active" href="/">Home</a></li>
+            <li><a class="nav-link scrollto" href="/about">About</a></li>
+            <li><a class="nav-link scrollto" href="/cards">Cards</a></li>
+            <li><a class="nav-link scrollto" href="/create">Create</a></li>
+            <li><a class="nav-link scrollto" href="/ideas">Ideas</a></li>
             <li class="dropdown">
               <a href="#">
-                <span>Drop Down</span>
+                <span>Account</span>
                 <i class="bi bi-chevron-down"></i>
               </a>
               <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li class="dropdown">
-                  <a href="#">
-                    <span>Deep Drop Down</span>
-                    <i class="bi bi-chevron-right"></i>
-                  </a>
-                  <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
+                <li><a href="/signup">Signup</a></li>
+                <li class="dropdown"></li>
+                <li><a href="/login">Login</a></li>
+                <li><a href="/login">Login</a></li>
               </ul>
             </li>
-            <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-            <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+            <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
+            <li><a class="getstarted scrollto" href="/crete">Get Started</a></li>
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
-        <!-- .navbar -->
       </div>
     </header>
 
@@ -174,16 +160,6 @@
   </div>
 
   <!-- <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/cards">Cards</router-link>
-      |
-      <router-link to="/create">Create</router-link>
-      |
-      <router-link to="/ideas">Ideas</router-link>
-      |
       <router-link to="/signup">Signup</router-link>
       |
       <router-link to="/login">Login</router-link>
