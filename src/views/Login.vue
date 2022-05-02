bc33a8292e635a79df99f58a2f53097b6141e558
Devorah Ortega <devorahb.ortega@gmail.com>
2022-05-01 22:00:13 -0500

    Adds bootstrap to login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,6 @@
       <div class="container" data-aos="fade-up">
         <div class="section-title">
           <br />
-          <br />
           <h2>Login</h2>
         </div>
 
@@ -28,61 +27,70 @@
                 <h4>Call:</h4>
                 <p>+1 5589 55488 55s</p>
               </div>
+
+              <!-- <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                frameborder="0"
+                style="border: 0; width: 100%; height: 290px"
+                allowfullscreen
+              ></iframe> -->
             </div>
           </div>
-          </section>
-          <section>
-          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch"> 
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
               <div class="row">
-              <div class="form-group">
-                <label for="name">Username</label>
+                <div class="form-group">
+                  <label for="name">Email</label>
                   <input
-             type="email"
-             class="form-control"
-             name="email"
-              id="email"
-             placeholder="email"
-             required
-             v-model="newSessionParams.email"
-                />
-              </div>
-         
-              <div class="form-group">
-                <label for="name">Subject</label>
-                <input
-            type="text"
-            class="form-control"
-            name="password"
-            id="password"
-            placeholder="Password"
-            required
-            v-model="newSessionParams.password"
-          />     <div class="form-group">
-                <label for="name">Subject</label>
-                <input
-            type="text"
-            class="form-control"
-            name="password_confirmation"
-            id="password_confirmation"
-            placeholder="Confirm Password"
-            required
-            v-model="newSessionParams.password"
-          />
-              </div>
+                    type="email"
+                    class="form-control"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    required
+                    v-model="newSessionParams.email"
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Password</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    required
+                    v-model="newSessionParams.password"
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Password Confirmation</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="password_confirmation"
+                    id="password_confirmation"
+                    placeholder="Confirm Password"
+                    required
+                    v-model="newSessionParams.password"
+                  />
+                </div>
               </div>
               <div class="my-3">
                 <div class="loading">Loading</div>
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-          </div> 
-        </form>
+              <div class="text-center"><button type="submit">Login</button></div>
+            </form>
+          </div>
+        </div>
       </div>
     </section>
+  </div>
 </template>
-        
+
 <script>
 import axios from "axios";
 export default {
