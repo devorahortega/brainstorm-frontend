2a17015b4cd91993865a833db899921d28d4095f
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-25 13:50:57 -0500

    Adds renovations to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,14 +5,17 @@
         <br />
         <br />
         <h2>About Us</h2>
+        <p>
+          Our app allows you to create cards and fill them with the ideas floating around in your mind. Getting
+          everything down on paper can help you straighten out your thoughts and emotions. And if you can't think
+          anything to write about we have a list of ideas to help you get you thinking.
+        </p>
+        <br />
+        <p>Below is a sample login so you can view all the aspects of the website without signing up for an account:</p>
       </div>
 
       <div class="row content">
         <div class="col-lg-6">
-          <p>
-            Our app allows you to create cards and fill them with the ideas floating around in your mind. Getting
-            everything down on paper can help you straighten out your thoughts and emotions.
-          </p>
           <ul>
             <li>
               <i class="ri-check-double-line"></i>
@@ -26,10 +29,31 @@
               <i class="ri-check-double-line"></i>
               Get ideas to write about
             </li>
+            <li>
+              <i class="ri-check-double-line"></i>
+              Jot down quick notes
+            </li>
+            <li>
+              <i class="ri-check-double-line"></i>
+              Record memories and contemplations
+            </li>
+            <li>
+              <i class="ri-check-double-line"></i>
+              Get to know yourself
+            </li>
           </ul>
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0">
-          <p>And if you can't think anything to write about we have a list of ideas to help you get you thinking.</p>
+          <div>
+            <h6>Email:</h6>
+            <p>sampleuser@example.com</p>
+          </div>
+          <br />
+          <div>
+            <h6>Password:</h6>
+            <p>password</p>
+          </div>
+          <br />
           <a href="/signup" class="btn-learn-more">Get Started</a>
         </div>
       </div>
