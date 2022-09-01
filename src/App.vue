eaaaeeffb7032b46ebb10ed3ee597ef9348213b8
Devorah Ortega <devorahb.ortega@gmail.com>
2022-09-01 13:09:57 -0500

    Removes contact page

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
                 <li><a href="/logout">Logout</a></li>
               </ul>
             </li>
-            <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
+            <!-- <li><a class="nav-link scrollto" href="/contact">Contact</a></li> -->
             <li><a class="getstarted scrollto" href="/create">Get Started</a></li>
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
